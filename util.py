150167794458018b2aa9da87e59655e0cac25722
NandaniAggarwal <nandaniaggarwal0109@gmail.com>
2025-11-06 20:41:44 +0530

    Update util.py

--- util.py
+++ util.py
@@ -24,15 +24,16 @@ def load_saved_artifacts():
     print("Loading saved artifacts")
     global __locations
     global __data_columns
-    with open("./artifcats/columns.json",'r') as f:
+    with open("./artifacts/columns.json",'r') as f:
         __data_columns=json.load(f)['data_columns']
         __locations=__data_columns[3:]
     global __model
     if __model is None:
-        with open("./artifcats/bangalore_home_prices_model.pickle",'rb') as f:
+        with open("./artifacts/bangalore_home_prices_model.pickle",'rb') as f:
          __model=pickle.load(f)
     print("Loaded saved artifacts ..done")
 if __name__ == '__main__':
     load_saved_artifacts()
     # print(get_estimated_price('1st Phase JP Nagar',1000,1,3))
+
     print(get_location_names())
